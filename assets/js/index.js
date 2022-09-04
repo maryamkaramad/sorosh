18f2e7443caa02d086048d63244d7afc3f0bc0a9
maryamkaramad <maryamkaramad1373@gmail.com>
2022-09-04 15:24:15 +0430

    blog and blogone finished

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,4 @@
 const burger_menu= document.querySelector("#burger_menu");
-console.log(burger_menu);
 const menu=document.querySelector("#menu");
-console.log(menu);
-
 menu.addEventListener("click", function (e) {
     burger_menu.classList.toggle("d-none") });
