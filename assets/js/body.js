effec9eb307ef29f4e607ff484d66ee2738113f6
maryamkaramad <maryamkaramad1373@gmail.com>
2022-09-05 22:55:07 +0430

    fix error3

--- assets/js/body.js
+++ assets/js/body.js
@@ -5,3 +5,11 @@ console.log(menu);
 
 menu.addEventListener("click", function (e) {
     burger_menu.classList.toggle("d-none") });
+
+
+
+    const header_mobile=document.querySelector("header_mobile");
+    const header_pc=document.querySelector("header_pc");
+
+
+  
